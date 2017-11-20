b5c95a267984018da4fe611b8aeca660d11bae85
Robert Craig <robertcraig38@gmail.com>
2017-11-20 12:13:43 -0500

    Worked on about

--- src/reducers/AboutReducer.js
+++ src/reducers/AboutReducer.js
@@ -2,13 +2,16 @@ import { ABOUT_EXPAND } from '../actions/types';
 import data from '../resources/json/about.json';
 
 const INITIAL_STATE = {
-  selected: 0,
+  selected: null,
   data
 };
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case ABOUT_EXPAND:
+      if (state.selected === action.payload) {
+        return { ...state, selected: null };
+      }
       return { ...state, selected: action.payload };
     default:
       return state;
--- src/screens/AboutScreen/AboutItem.js
+++ src/screens/AboutScreen/AboutItem.js
@@ -1,40 +1,132 @@
 import React, { Component } from 'react';
 import {
   LayoutAnimation,
-  UIManager
+  UIManager,
+  View,
+  FlatList,
+  StyleSheet,
 } from 'react-native';
 import { connect } from 'react-redux';
 import {
-  Card,
-  CardItem,
+  ListItem,
   Text,
 } from 'native-base';
 import { aboutExpand } from '../../actions';
 
 UIManager.setLayoutAnimationEnabledExperimental(true);
 
-class ListItem extends Component {
+class AboutItem extends Component {
   componentWillUpdate() {
     LayoutAnimation.easeInEaseOut();
   }
 
-  render() {
+  renderDescription({ item }) {
     return (
-      <CardItem>
-        <Text>{this.props.item.title}</Text>
-      </CardItem>
+      <ListItem>
+        <Text>{item.text}</Text>
+      </ListItem>
+    );
+  }
+
+  renderPoints() {
+    const { pointsTitle, points } = this.props.item;
+    if (!pointsTitle) {
+      return;
+    }
+    return (
+      <View>
+        <ListItem>
+          <Text>{pointsTitle}</Text>
+        </ListItem>
+        <FlatList
+          data={points}
+          renderItem={this.renderPoint.bind(this)}
+          contentContainerStyle={{ padding: 10 }}
+        />
+      </View>
+    );
+  }
+
+  renderPoint({ item }) {
+    return (
+      <ListItem>
+        <Text>{item.text}</Text>
+      </ListItem>
+    );
+  }
+
+  renderContent() {
+    const { item, expanded } = this.props;
+    if (!expanded) {
+      return;
+    }
+    return (
+      <View style={{ flex: 1 }}>
+        <FlatList
+          data={item.description}
+          renderItem={this.renderDescription.bind(this)}
+          contentContainerStyle={{ padding: 10 }}
+        />
+        {this.renderPoints()}
+      </View>
+    );
+  }
+
+  render() {
+    const { titleItem, titleItemActive, titleText } = styles;
+    const { title, key } = this.props.item;
+    return (
+      <View>
+        <ListItem
+          style={(this.props.expanded) ? [titleItem, titleItemActive] : titleItem}
+          button
+          onPress={() => this.props.aboutExpand(key)}
+        >
+          <Text style={titleText}>{title}</Text>
+        </ListItem>
+        {this.renderContent()}
+      </View>
     );
   }
 }
 
-const styles = {
+const styles = StyleSheet.create({
+  titleItem: {
+    borderLeftWidth: 5,
+    borderLeftColor: '#ae263d',
+    height: 50,
+    paddingLeft: 10,
+  },
+  titleItemActive: {
+    borderLeftColor: '#212121',
+  },
+  titleText: {
+    fontSize: 20,
+  },
+  descriptionList: {
 
-};
+  },
+  descriptionItem: {
 
-const mapStateToProps = (state, ownProps) => {
-  const expanded = state.selectedLibraryId === ownProps.item.key;
+  },
+  descriptionText: {
+
+  },
+  pointsList: {
+
+  },
+  pointsTitle: {
+
+  },
+  pointsText: {
+
+  },
+});
+
+const mapStateToProps = ({ about }, ownProps) => {
+  const expanded = about.selected === ownProps.item.key;
 
   return { expanded };
 };
 
-export default connect(mapStateToProps, { aboutExpand })(ListItem);
+export default connect(mapStateToProps, { aboutExpand })(AboutItem);
